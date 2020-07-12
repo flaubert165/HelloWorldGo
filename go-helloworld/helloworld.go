c392f1f74974646fb845046e2f7ad3d31596c951
Italo Flauber Cavalcanti de Santana <italo.santana@xpi.com.br>
2020-07-12 14:52:52 -0300

    init

---
+++ go-helloworld/helloworld.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/api/hello-world", helloworld)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+func helloworld(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(map[string]string{"ok": "hello world!!!"})
+}
